3ff40ea7269fc8366b84a0badbf7f0f82d14d79f
Matt Holmes <holmesm8@gmail.com>
2020-03-17 13:31:48 -0600

    Fix tests to include invoice has many transactions

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -8,8 +8,10 @@ RSpec.describe Invoice, type: :model do
 
   describe 'relationships' do 
     it { should belong_to :customer}
+    it { should belong_to :merchant}
     it { should have_many :invoice_items}
     it { should have_many(:items).through(:invoice_items) }
+    it { should have_many :transactions}
   end 
 
   # describe 'methods' do
