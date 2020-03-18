e8ddc4f0b7cf5dbf3a86b5851d0380e14e366053
Matt Holmes <holmesm8@gmail.com>
2020-03-18 17:04:22 -0600

    Refactor to disregard params require

--- app/serializers/merchant_serializer.rb
+++ app/serializers/merchant_serializer.rb
@@ -2,6 +2,4 @@ class MerchantSerializer
   include FastJsonapi::ObjectSerializer
   attributes :id, :name
 
-  has_many :items
-  has_many :invoices
 end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -30,7 +30,7 @@ describe "Merchants API" do
   it "can create a new merchant" do
     merchant_params = { name: "Matt's Store" }
 
-    post "/api/v1/merchants", params: {merchant: merchant_params}
+    post "/api/v1/merchants", params: merchant_params
     merchant = Merchant.last
 
     expect(response).to be_successful
@@ -43,7 +43,7 @@ describe "Merchants API" do
     previous_name = Merchant.last.name
     merchant_params = { name: "Matt's Store" }
 
-    put "/api/v1/merchants/#{id}", params: {merchant: merchant_params}
+    put "/api/v1/merchants/#{id}", params: merchant_params
     merchant = Merchant.find_by(id: id)
 
     expect(response).to be_successful
