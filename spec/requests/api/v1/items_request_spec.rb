eb9a3eb5a76c57c322670a3641d47f326ce042b5
Matt Holmes <holmesm8@gmail.com>
2020-03-17 18:59:21 -0600

    Add testing for items show

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -7,12 +7,26 @@ describe "Items API" do
     item2 = create(:item, merchant_id: merchant1.id)
     item3 = create(:item, merchant_id: merchant1.id)
 
-    get '/api/v1/items'
+    get "/api/v1/items"
 
     expect(response).to be_successful
 
     items = JSON.parse(response.body)
     expect(items.count).to eq(3)
+  end
+
+  it "sends a list of items" do
+    merchant1 = create(:merchant)
+    item1 = create(:item, merchant_id: merchant1.id)
+    item2 = create(:item, merchant_id: merchant1.id)
+    item3 = create(:item, merchant_id: merchant1.id)
+
+    get "/api/v1/items/#{item1.id}"
+
+    item = JSON.parse(response.body)
+
+    expect(response).to be_successful
+    expect(item[:id]).to eq(id)
   end
 
 end
