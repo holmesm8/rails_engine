b381fb1979a6b4d46c29f036e1996baf8659797e
Matt Holmes <holmesm8@gmail.com>
2020-03-17 23:03:48 -0600

    Add method unit_price_converter to item model

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,6 +1,10 @@
 class ItemSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :id, :name, :description, :unit_price, :merchant_id
+  attributes :id, :name, :description, :merchant_id
 
+  attribute :unit_price do |item|
+    item.unit_price_converter
+  end
+  
   belongs_to :merchant
 end
