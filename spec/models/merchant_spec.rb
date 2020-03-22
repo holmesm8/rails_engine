a16e67ab84d2d880b8e82a368f51c600a4aa9672
Matt Holmes <holmesm8@gmail.com>
2020-03-22 11:10:22 -0600

    Add testing for business intelligence date range

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -12,29 +12,29 @@ RSpec.describe Merchant, type: :model do
 
   describe 'methods' do
     before(:each) do 
-      @customer1 = create(:customer)
+      @customer1 = create(:customer, created_at: "2012-03-16")
 
-      @merchant1 = create(:merchant)
+      @merchant1 = create(:merchant, created_at: "2012-03-16")
       @merchant2 = create(:merchant)
       @merchant3 = create(:merchant)
       @merchant4 = create(:merchant)
 
-      @item1 = create(:item, unit_price: 1, merchant_id: @merchant1.id)
+      @item1 = create(:item, unit_price: 1, merchant_id: @merchant1.id, created_at: "2012-03-16")
       @item2 = create(:item, unit_price: 2, merchant_id: @merchant2.id)
       @item3 = create(:item, unit_price: 3, merchant_id: @merchant3.id)
       @item4 = create(:item, unit_price: 4, merchant_id: @merchant4.id)
 
-      @invoice1 = create(:invoice, customer_id: @customer1.id, merchant_id: @merchant1.id)
-      @invoice2 = create(:invoice, customer_id: @customer1.id, merchant_id: @merchant2.id)
-      @invoice3 = create(:invoice, customer_id: @customer1.id, merchant_id: @merchant3.id)
-      @invoice4 = create(:invoice, customer_id: @customer1.id, merchant_id: @merchant4.id)
+      @invoice1 = create(:invoice, customer_id: @customer1.id, merchant_id: @merchant1.id, created_at: "2012-03-16")
+      @invoice2 = create(:invoice, customer_id: @customer1.id, merchant_id: @merchant2.id, created_at: "2020-03-16")
+      @invoice3 = create(:invoice, customer_id: @customer1.id, merchant_id: @merchant3.id, created_at: "2020-03-16")
+      @invoice4 = create(:invoice, customer_id: @customer1.id, merchant_id: @merchant4.id, created_at: "2020-03-16")
 
-      @invoice_items1 = create(:invoice_items, item_id: @item1.id, quantity: 1, unit_price: 1, invoice_id: @invoice1.id)
+      @invoice_items1 = create(:invoice_items, item_id: @item1.id, quantity: 1, unit_price: 1, invoice_id: @invoice1.id, created_at: "2012-03-16")
       @invoice_items2 = create(:invoice_items, item_id: @item2.id, quantity: 1, unit_price: 2, invoice_id: @invoice2.id)
       @invoice_items3 = create(:invoice_items, item_id: @item3.id, quantity: 1, unit_price: 3, invoice_id: @invoice3.id)
       @invoice_items4 = create(:invoice_items, item_id: @item4.id, quantity: 1, unit_price: 4, invoice_id: @invoice4.id)
 
-      @transaction1 = create(:transaction, invoice_id: @invoice1.id)
+      @transaction1 = create(:transaction, invoice_id: @invoice1.id, created_at: "2012-03-16")
       @transaction2 = create(:transaction, invoice_id: @invoice2.id)
       @transaction3 = create(:transaction, invoice_id: @invoice3.id)
       @transaction4 = create(:transaction, invoice_id: @invoice4.id)
@@ -51,5 +51,15 @@ RSpec.describe Merchant, type: :model do
 
       expect(Merchant.merchant_revenue(@merchant1.id)).to eq(expected_results)
     end
+
+    it "#total_revenue_by_date_range" do
+      start_date = "2012-03-15"
+      end_date = "2013-12-01"
+
+      expected_results = 1
+
+      expect(Merchant.total_revenue_by_date_range(start_date.to_date, end_date.to_date)).to eq(1)
+    end
   end
 end
+
--- spec/requests/api/v1/merchants_business_intelligence_spec.rb
+++ spec/requests/api/v1/merchants_business_intelligence_spec.rb
@@ -2,29 +2,29 @@ require 'rails_helper'
 
 describe "Merchants Business Intelligence" do 
   before(:each) do 
-    @customer1 = create(:customer)
+    @customer1 = create(:customer, created_at: "2012-03-16")
 
-    @merchant1 = create(:merchant)
+    @merchant1 = create(:merchant, created_at: "2012-03-16")
     @merchant2 = create(:merchant)
     @merchant3 = create(:merchant)
     @merchant4 = create(:merchant)
 
-    @item1 = create(:item, unit_price: 1, merchant_id: @merchant1.id)
+    @item1 = create(:item, unit_price: 1, merchant_id: @merchant1.id, created_at: "2012-03-16")
     @item2 = create(:item, unit_price: 2, merchant_id: @merchant2.id)
     @item3 = create(:item, unit_price: 3, merchant_id: @merchant3.id)
     @item4 = create(:item, unit_price: 4, merchant_id: @merchant4.id)
 
-    @invoice1 = create(:invoice, customer_id: @customer1.id, merchant_id: @merchant1.id)
+    @invoice1 = create(:invoice, customer_id: @customer1.id, merchant_id: @merchant1.id, created_at: "2012-03-16")
     @invoice2 = create(:invoice, customer_id: @customer1.id, merchant_id: @merchant2.id)
     @invoice3 = create(:invoice, customer_id: @customer1.id, merchant_id: @merchant3.id)
     @invoice4 = create(:invoice, customer_id: @customer1.id, merchant_id: @merchant4.id)
 
-    @invoice_items1 = create(:invoice_items, item_id: @item1.id, quantity: 1, unit_price: 1, invoice_id: @invoice1.id)
+    @invoice_items1 = create(:invoice_items, item_id: @item1.id, quantity: 1, unit_price: 1, invoice_id: @invoice1.id, created_at: "2012-03-16")
     @invoice_items2 = create(:invoice_items, item_id: @item2.id, quantity: 1, unit_price: 2, invoice_id: @invoice2.id)
     @invoice_items3 = create(:invoice_items, item_id: @item3.id, quantity: 1, unit_price: 3, invoice_id: @invoice3.id)
     @invoice_items4 = create(:invoice_items, item_id: @item4.id, quantity: 1, unit_price: 4, invoice_id: @invoice4.id)
 
-    @transaction1 = create(:transaction, invoice_id: @invoice1.id)
+    @transaction1 = create(:transaction, invoice_id: @invoice1.id, created_at: "2012-03-16")
     @transaction2 = create(:transaction, invoice_id: @invoice2.id)
     @transaction3 = create(:transaction, invoice_id: @invoice3.id)
     @transaction4 = create(:transaction, invoice_id: @invoice4.id)
@@ -47,5 +47,16 @@ describe "Merchants Business Intelligence" do
     merchant_revenue = JSON.parse(response.body, symbolize_names: true)[:data]
 
     expect(merchant_revenue[:attributes][:revenue]).to eq(1)
+  end
+
+  it "returns the total revenue across date range" do
+    get "/api/v1/revenue?start=2012-03-09&end=2012-03-24"
+
+    expect(response).to be_successful
+    
+    merchant_revenue = JSON.parse(response.body, symbolize_names: true)[:data]
+    require 'pry'; binding.pry
+
+    expect(merchant_revenue[:attributes][:revenue].to_f.round(2)).to eq(1)
   end
 end
