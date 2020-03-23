1a8f970cfc79212af1156689973214becb15e783
Matt Holmes <holmesm8@gmail.com>
2020-03-23 08:23:05 -0600

    Final commit

--- app/controllers/api/v1/revenue_controller.rb
+++ app/controllers/api/v1/revenue_controller.rb
@@ -1,13 +1,7 @@
 class Api::V1::RevenueController < ApplicationController
   def show
-    render json: {data: {id: params[:id].to_i, attributes: {revenue: total_revenue}}}
+    render json: {data: {id: nil , attributes: {revenue: total_revenue}}}
   end
-  
-  private
-
-    def find_params
-      params.permit(:id, :name, :created_at, :updated_at)
-    end
 
   def total_revenue
     start_date = params[:start].to_date
--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -4,5 +4,5 @@ class InvoiceItem < ApplicationRecord
 
   belongs_to :item
   belongs_to :invoice
-
+  has_many :transactions, through: :invoice
 end
